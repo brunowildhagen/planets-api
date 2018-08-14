561c47ed0e60b4f94c32b7605836ad2b7ecfd3fb
Bruno Alves Wildhagen <bruno.wildhagen@minds.local>
2018-08-14 13:32:16 -0300

    Base structure

---
+++ config/db.js
@@ -0,0 +1,23 @@
+'use strict';
+require("../models/planetModel");
+
+const mongoose = require("mongoose");
+
+const dbURI =
+  "mongodb://localhost/planet";
+
+const config = {
+  reconnectTries: Number.MAX_VALUE,
+  poolSize: 10
+};
+
+mongoose.connect(dbURI, config).then(
+  function() {
+    console.log("Conexão com o banco de dados bem sucedida!");
+  },
+  function(err) {
+    console.log("Erro ao conectar com a base de dados: ", err);
+  }
+);
+
+// require any models
--- models/planetModel.js
+++ models/planetModel.js
@@ -5,16 +5,20 @@ var Schema = mongoose.Schema;
 var planet = new Schema({
     nome:{
         type:String,
-        required:"O planeta deve ter um nome"
+        required:"O planeta deve ter um nome."
     },
     clima:{
         type:String,
-        required:"O planeta deve ter um clima"
+        required:"O planeta deve ter um clima."
     },
     terreno:{
         type:String,
-        required:"O planeta deve ter um tipo de terreno"
-    }
+        required:"O planeta deve ter um tipo de terreno."
+    },
+	featured:[{
+		type:Number,
+		required:"Digite o número de aparições do planeta em filmes."
+	}]
 });
 
 module.exports = mongoose.model("Planets", planet);
